8801cb32c40877887b0ef3a810a43e9d04e8bdcf
ryieeeeee <ryzaquino04@gmail.com>
2024-10-02 19:19:56 +0800

    hi

--- script.js
+++ script.js
@@ -1,11 +1,25 @@
 function changeImage(drink) {
     const image = document.getElementById('display-image');
     if (drink === 'coffee') {
-        image.src = 'TCM(logo).png'; //tinry ko lang lagyan ng photo to //
+        image.src = 'menu1COFFEE.jpg'; //tinry ko lang lagyan ng photo to //
     } else if (drink === 'tea') {
-        image.src = 'tea.jpg'; 
+        image.src = 'menu2TEA.jpg'; 
     } else if (drink === 'matcha') {
-        image.src = 'matcha.jpg'; 
+        image.src = 'menu3MATCHA.jpg'; 
+    } else if (drink === 'soda') {
+        image.src = 'menu4SODA.jpg'; 
+    } else if (drink === 'juice') {
+        image.src = 'menu5JUICE.jpg'; 
+    } else if (drink === 'sandwich') {
+        image.src = 'menu7SANDWICH.jpg'; 
+    } else if (drink === 'pasta') {
+        image.src = 'menu8PASTA.jpg'; 
+    } else if (drink === 'pastry') {
+        image.src = 'menu9PASTRY.jpg'; 
+    } else if (drink === 'potato') {
+        image.src = 'menu91POTATO.jpg'; 
+    } else if (drink === 'milkchoco') {
+        image.src = 'menu6MILK&CHOCO.jpg'; 
     } else {
         image.src = ''; //lagay nalang kayo kung ano image gusto niyo i display//
     }
